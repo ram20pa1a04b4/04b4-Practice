180ddfff09c4a39c005b9e50d6feadf03e20f002
ram20pa1a04b4 <124040841+ram20pa1a04b4@users.noreply.github.com>
2023-02-07 19:15:46 +0530

    Create Student Normal.java

---
+++ one mon/Student Normal.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public class Students{
+      private String  Name;
+      private int RollNo;
+      private int Year;
+      private String Branch;
+      private String List of marks;
+        Students(String  Name,int RollNo,int Year,String Branch){
+        public String getName(){
+            return Name ;
+        }
+        public void setName(String s){
+            this.Name = s;
+        }
+        public int getRollNo(){
+            return RollNo ;
+        }
+        public void setRollNo(String s){
+            this.RollNo = s;
+        }
+        public int getYear(){
+            return Year ;
+        }
+        public void setYear(String s){
+            this.Year = s;
+        }
+        public String getBranch(){
+            return Branch ;
+        }
+        public void setBranch(String s){
+            this.Branch = s;
+        }
+      }
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
